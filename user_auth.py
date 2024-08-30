78becb6fecf1e0ad639468df6d79368c87ffb06b
AyaanZ30 <161135651+AyaanZ30@users.noreply.github.com>
2024-08-30 08:53:14 +0530

    Delete user_auth.py

--- user_auth.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-def authenticate_user():
-    st.sidebar.title("User Authentication")
-    user_id = st.sidebar.text_input("Enter your user ID:")
-    if not user_id:
-        st.warning("Please enter a user ID to continue.")
-        return None
-    return user_id
