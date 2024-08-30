8e5be74bef9a181c72e15b2098b2fcce3db2e753
AyaanZ30 <161135651+AyaanZ30@users.noreply.github.com>
2024-08-30 08:50:01 +0530

    Update ml_models.py

--- ml_models.py
+++ ml_models.py
@@ -6,11 +6,11 @@ from keras.models import Sequential
 from keras.layers import Dense, LSTM
 
 def predict_stock_price_lstm(data, days=30):
-    # Prepare data
+    
     scaler = MinMaxScaler(feature_range=(0,1))
     scaled_data = scaler.fit_transform(data['Close'].values.reshape(-1,1))
 
-    # Create training dataset
+   
     training_data_len = int(np.ceil( len(scaled_data) * .95 ))
     train_data = scaled_data[0:training_data_len, :]
     x_train, y_train = [], []
@@ -22,7 +22,7 @@ def predict_stock_price_lstm(data, days=30):
     x_train, y_train = np.array(x_train), np.array(y_train)
     x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
 
-    # Build LSTM model
+   
     model = Sequential()
     model.add(LSTM(50, return_sequences=True, input_shape=(x_train.shape[1], 1)))
     model.add(LSTM(50, return_sequences=False))
@@ -32,7 +32,7 @@ def predict_stock_price_lstm(data, days=30):
     model.compile(optimizer='adam', loss='mean_squared_error')
     model.fit(x_train, y_train, batch_size=1, epochs=1)
 
-    # Create testing dataset
+   
     test_data = scaled_data[training_data_len - 60:, :]
     x_test = []
     for i in range(60, len(test_data)):
@@ -41,11 +41,11 @@ def predict_stock_price_lstm(data, days=30):
     x_test = np.array(x_test)
     x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
 
-    # Make predictions
+   
     predictions = model.predict(x_test)
     predictions = scaler.inverse_transform(predictions)
 
-    # Predict future prices
+    
     last_60_days = scaled_data[-60:]
     X_future = []
     for i in range(days):
